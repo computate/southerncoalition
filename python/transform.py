3652ea4dd975e3346288192740ad2db7acfce1c3
Kyle Benson <kyleabenson@gmail.com>
2020-08-19 13:03:40 -0400

    Cleaned up python code, sample JSON output attach

--- python/transform.py
+++ python/transform.py
@@ -6,18 +6,34 @@ import numpy as np
 import pandas as pd
 import os
 
-
-
 with open('Pupils_by_Race_and_Sex.xls', 'r') as f:
     contents = f.read()
 
 soup = BeautifulSoup(contents, 'lxml')
 
-
 df = pd.read_html(str(soup), keep_default_na=False, header=0)
 
+## Read_html places everything under a single array, so we rework the dataframe to look/work at the top level normally, and rework the naming conventions
+df = df[0].rename(columns={"Year": "reportCardStartYear","____LEA Name____" : "agency", "INDIAN Male" : "pupilsIndianMale", "INDIAN Female" : "pupilsIndianFemale", "ASIAN Male" : "pupilsAsianMale", "ASIAN Female" : "pupilsAsianFemale", "HISPANIC Male" : "pupilsHispanicMale", "HISPANIC Female" : "pupilsHispanicFemale", "BLACK Male" : "pupilsBlackMale", "BLACK Female" : "pupilsBlackFemale", "WHITE Male" : "pupilsWhiteMale", "WHITE Female" : "pupilsWhiteFemale", "PACIFIC ISLAND\u00a0 Male" : "pupilsPacificIslanderMale", "PACIFIC ISLAND\u00a0 Female" : "pupilsPacificIslanderFemale", "TWO OR MORE Male" : "pupilsMultiRacialMale", "TWO OR MOREFemale" : "pupilsMultiRacialFemale"})
+df.insert(1, "pk", df["reportCardStartYear"]+df["LEA"] )
 
-df_revised = df[0].rename(columns={"Year": "reportCardStartYear", "INDIAN Male" : "pupilsIndianMale", "INDIAN Female" : "pupilsIndianFemale", "ASIAN Male" : "pupilsAsianMale", "ASIAN Female" : "pupilsAsianFemale", "HISPANIC Male" : "pupilsHispanicMale", "HISPANIC Female" : "pupilsHispanicFemale", "BLACK Male" : "pupilsBlackMale", "BLACK Female" : "pupilsBlackFemale", "WHITE Male" : "pupilsWhiteMale", "WHITE Female" : "pupilsWhiteFemale", "PACIFIC ISLAND  Male" : "pupilsPacificIslanderMale", "PACIFIC ISLAND  Female" : "pupilsPacificIslanderFemale", "TWO OR MORE Male" : "pupilsMultiRacialMale", "TWO OR MOREFemale" : "pupilsMultiRacialFemale"})
+saves_array = [ "pupilsIndianFemale", "reportCardStartYear", "pupilsPacificIslanderMale", "pupilsWhiteFemale", "inheritPk", "pupilsHispanicFemale", "pupilsAsianFemale", "pupilsHispanicMale", "pupilsBlackMale", "userId", "pupilsMultiRacialMale", "pupilsIndianMale", "pupilsWhiteMale", "pupilsMultiRacialFemale", "pupilsBlackFemale", "pupilsTotal", "pupilsAsianMale", "pupilsPacificIslanderFemale", "countyKey" ]
 
 
-df_revised.to_json('results.json', orient='index')
+results = df.to_json( orient='index')
+python_dict = json.loads(results)
+
+
+
+final_result = { "list": [] }
+for i in python_dict:
+    final_result["list"].append(python_dict[i])
+
+for i in final_result["list"]:
+    i.update({"saves" : saves_array})
+
+with open('results.json', 'w') as json_file:
+  json.dump(final_result, json_file, indent=4)
+
+
+
