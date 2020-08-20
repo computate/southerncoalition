98c19e12db6ab84514dba15f1027d6fd42ee8229
ctate <computate@computate.org>
2020-08-20 16:28:36 -0600

    Adding some updates to populate agencyKey, rename some columns and add a missing requirement.

--- python/transform.py
+++ python/transform.py
@@ -14,8 +14,9 @@ soup = BeautifulSoup(contents, 'lxml')
 df = pd.read_html(str(soup), keep_default_na=False, header=0)
 
 ## Read_html places everything under a single array, so we rework the dataframe to look/work at the top level normally, and rework the naming conventions
-df = df[0].rename(columns={"Year": "reportCardStartYear","____LEA Name____" : "agency", "INDIAN Male" : "pupilsIndianMale", "INDIAN Female" : "pupilsIndianFemale", "ASIAN Male" : "pupilsAsianMale", "ASIAN Female" : "pupilsAsianFemale", "HISPANIC Male" : "pupilsHispanicMale", "HISPANIC Female" : "pupilsHispanicFemale", "BLACK Male" : "pupilsBlackMale", "BLACK Female" : "pupilsBlackFemale", "WHITE Male" : "pupilsWhiteMale", "WHITE Female" : "pupilsWhiteFemale", "PACIFIC ISLAND\u00a0 Male" : "pupilsPacificIslanderMale", "PACIFIC ISLAND\u00a0 Female" : "pupilsPacificIslanderFemale", "TWO OR MORE Male" : "pupilsMultiRacialMale", "TWO OR MOREFemale" : "pupilsMultiRacialFemale"})
+df = df[0].rename(columns={"Year": "reportCardStartYear","____LEA Name____" : "agency", "INDIAN Male" : "pupilsIndianMale", "INDIAN Female" : "pupilsIndianFemale", "ASIAN Male" : "pupilsAsianMale", "ASIAN Female" : "pupilsAsianFemale", "HISPANIC Male" : "pupilsHispanicMale", "HISPANIC Female" : "pupilsHispanicFemale", "BLACK Male" : "pupilsBlackMale", "BLACK Female" : "pupilsBlackFemale", "WHITE Male" : "pupilsWhiteMale", "WHITE Female" : "pupilsWhiteFemale", "PACIFIC ISLAND\u00a0 Male" : "pupilsPacificIslanderMale", "PACIFIC ISLAND Male" : "pupilsPacificIslanderMale", "PACIFIC ISLAND\u00a0 Female" : "pupilsPacificIslanderFemale", "PACIFIC ISLAND Female" : "pupilsPacificIslanderFemale", "TWO OR MORE Male" : "pupilsMultiRacialMale", "TWO OR MOREFemale" : "pupilsMultiRacialFemale"})
 df.insert(1, "pk", df["reportCardStartYear"]+df["LEA"] )
+df.insert(1, "agencyKey", df["LEA"] )
 
 saves_array = [ "pupilsIndianFemale", "reportCardStartYear", "pupilsPacificIslanderMale", "pupilsWhiteFemale", "inheritPk", "pupilsHispanicFemale", "pupilsAsianFemale", "pupilsHispanicMale", "pupilsBlackMale", "userId", "pupilsMultiRacialMale", "pupilsIndianMale", "pupilsWhiteMale", "pupilsMultiRacialFemale", "pupilsBlackFemale", "pupilsTotal", "pupilsAsianMale", "pupilsPacificIslanderFemale", "agencyKey" ]
 
@@ -27,7 +28,8 @@ python_dict = json.loads(results)
 
 final_result = { "list": [] }
 for i in python_dict:
-    final_result["list"].append(python_dict[i])
+    if python_dict[i]["agencyKey"]:
+        final_result["list"].append(python_dict[i])
 
 for i in final_result["list"]:
     i.update({"saves" : saves_array})
